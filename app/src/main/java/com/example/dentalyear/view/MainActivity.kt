c4170af82e87ea0799aa0ec060e5a21192f953e4
Mohamed Selim <dev.m.selim@gmail.com>
2020-11-28 11:22:28 +0300

    define project structure

--- app/src/main/java/com/example/dentalyear/view/MainActivity.kt
+++ app/src/main/java/com/example/dentalyear/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.dentalyear
+package com.example.dentalyear.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dentalyear.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
