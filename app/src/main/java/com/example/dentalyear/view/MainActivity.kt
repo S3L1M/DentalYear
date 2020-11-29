cc12e5aa60153843b0e510501706f03da9e81356
Mohamed Selim <dev.m.selim@gmail.com>
2020-11-29 05:59:45 +0300

    Implement navigation bar functionality

--- app/src/main/java/com/example/dentalyear/view/MainActivity.kt
+++ app/src/main/java/com/example/dentalyear/view/MainActivity.kt
@@ -2,11 +2,81 @@ package com.example.dentalyear.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation
 import com.example.dentalyear.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var nav: MeowBottomNavigation
+    private lateinit var mHomeTextView:TextView
+    private lateinit var mNotesTextView:TextView
+    private lateinit var mExhibitsTextView:TextView
+    private lateinit var mVideoTextView:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        this.supportActionBar?.hide()
+
+        // get TextViews by IDs
+        mHomeTextView = findViewById(R.id.tv_home)
+        mNotesTextView = findViewById(R.id.tv_notes)
+        mExhibitsTextView = findViewById(R.id.tv_exhibits)
+        mVideoTextView = findViewById(R.id.tv_video)
+
+        // Build nav bar
+        nav = findViewById(R.id.bottom_nav)
+        nav.add(MeowBottomNavigation.Model(1, R.drawable.ic_baseline_home_24))
+        nav.add(MeowBottomNavigation.Model(2, R.drawable.ic_baseline_edit_24))
+        nav.add(MeowBottomNavigation.Model(3, R.drawable.ic_baseline_shopping_cart_24))
+        nav.add(MeowBottomNavigation.Model(4, R.drawable.ic_baseline_play_arrow_24))
+
+        // onClickMenuListener event
+        nav.setOnClickMenuListener {
+            when(it.id) {
+                1 -> showHome()
+                2 -> showNotes()
+                3 -> showExhibits()
+                4 -> showVideo()
+            }
+        }
+
+        // Select Home
+        nav.show(1)
+        setSelectedTextColor(mHomeTextView)
+
+    }
+
+    private fun showHome() {
+        setSelectedTextColor(mHomeTextView)
+
+        // Inflate fragment here
+    }
+
+    private fun showNotes() {
+        setSelectedTextColor(mNotesTextView)
+
+        // Inflate fragment here
+    }
+
+    private fun showExhibits() {
+        setSelectedTextColor(mExhibitsTextView)
+
+        // Inflate fragment here
+    }
+
+    private fun showVideo() {
+        setSelectedTextColor(mVideoTextView)
+
+        // Inflate fragment here
+    }
+
+    private fun setSelectedTextColor(tv:TextView) {
+        val defaultColor: Int = getColor(android.R.color.tab_indicator_text)
+        mHomeTextView.setTextColor(defaultColor)
+        mNotesTextView.setTextColor(defaultColor)
+        mExhibitsTextView.setTextColor(defaultColor)
+        mVideoTextView.setTextColor(defaultColor)
+        tv.setTextColor(getColor(R.color.selectedText))
     }
 }
