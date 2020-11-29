037eee3b1c535d40f99e1206607bbe3cc4fe1978
YoHaNoMe <yoyomb2010@gmail.com>
2020-11-29 19:23:17 +0200

    init homeRecyclerView | modify fragment_home | change ids of home_recycler_view_item and exbandable_item

--- app/src/main/java/com/example/dentalyear/view/HomeFragment.kt
+++ app/src/main/java/com/example/dentalyear/view/HomeFragment.kt
@@ -5,7 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.dentalyear.R
+import com.example.dentalyear.data.model.HomeData
+import com.example.dentalyear.view.adapter.HomeAdapter
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -15,6 +19,24 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // init recyclerView
+        home_recycler_view.adapter = HomeAdapter(populateDummyData(), home_recycler_view)
+
+    }
+
+    private fun populateDummyData(): List<HomeData>{
+        val dummyData: MutableList<HomeData> = mutableListOf()
+
+        for(i in 1..10){
+            val homeData = HomeData("Title: $i", "Description: $i", R.drawable.analysis)
+            dummyData.add(homeData)
+        }
+        return dummyData
     }
 
 }
