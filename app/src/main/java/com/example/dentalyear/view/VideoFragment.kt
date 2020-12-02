eb8e652c8e38f32ad8ba9c0a9b6cc450614faf4b
YoHaNoMe <yoyomb2010@gmail.com>
2020-12-02 04:05:14 +0200

    add VideoFragment and VideoAdapter with all xmls needed

--- app/src/main/java/com/example/dentalyear/view/VideoFragment.kt
+++ app/src/main/java/com/example/dentalyear/view/VideoFragment.kt
@@ -1,5 +1,6 @@
 package com.example.dentalyear.view
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -7,12 +8,29 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.dentalyear.R
+import com.example.dentalyear.data.model.VideoModel
+import com.example.dentalyear.utils.Status
+import com.example.dentalyear.view.adapter.VideoAdapter
 import com.example.dentalyear.viewmodel.MainViewModel
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import kotlinx.android.synthetic.main.fragment_video.*
 
 
 class VideoFragment : Fragment() {
+
+    companion object {
+        private const val PLAYBACK_TIME = "playback_time"
+        private const val PLAY_WHEN_READY = "play_when_ready"
+    }
+
+    private var playWhenReady: Boolean = true
+    private var playBackPosition: Long = 0
+    private var videosList: List<VideoModel>? = listOf()
+    private lateinit var adapter: VideoAdapter
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,14 +43,100 @@ class VideoFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Get the current position and play/pause | if there is any configuration changes
+        savedInstanceState?.let {
+            playBackPosition = it.getLong(PLAYBACK_TIME)
+            playWhenReady = it.getBoolean(PLAY_WHEN_READY)
+        }
+
+        // Define ViewModel
         val viewModel: MainViewModel by viewModels()
-        val videos = viewModel.getVideos().observe(viewLifecycleOwner, {videos->
-            if(videos == null)
-                return@observe
-            Log.d("VideoFragment", "Status Data: ${videos.status}")
-            Log.d("VideoFragment", "Message: ${videos.message}")
-            Log.d("VideoFragment", "${videos.data?.size}")
+
+
+        /**
+         * Init Visibility of the views
+         * By default the views will be HIDDEN until the data become available
+         */
+        changeViewVisibility(View.INVISIBLE)
+
+        // Init Adapter
+        initRecyclerViewAdapter()
+
+        // Define media controller and attach it to videoView
+//        initMediaPlayerController()
+
+        // Get Videos
+        viewModel.getVideos().observe(viewLifecycleOwner, { videos ->
+            if (videos.status == Status.LOADING) {
+                video_fragment_progressbar.visibility = View.VISIBLE
+            } else if (videos.status == Status.SUCCESS) {
+                video_fragment_progressbar.visibility = View.INVISIBLE
+                changeViewVisibility(View.VISIBLE)
+                Log.d("VideoFragment", "Status Data: ${videos.status}")
+                Log.d("VideoFragment", "Message: ${videos.message}")
+                Log.d("VideoFragment", "${videos.data?.size}")
+                videosList = videos.data
+                adapter.setData(videos.data)
+                initPlayer("https://developers.google.com/training/images/tacoma_narrows.mp4")
+            }
         })
     }
 
+    private fun changeViewVisibility(visibility: Int) {
+        video_fragment_category_options_text_view.visibility = visibility
+        video_fragment_category_options_subtitle_text_view.visibility = visibility
+        video_fragment_save_image_button.visibility = visibility
+        video_fragment_tabLayout.visibility = visibility
+        video_fragment_content_container.visibility = visibility
+    }
+
+    private fun initRecyclerViewAdapter() {
+        video_fragment_recycler_view.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        adapter = VideoAdapter()
+        video_fragment_recycler_view.adapter = adapter
+    }
+
+    private fun initPlayer(url: String) {
+        val player = SimpleExoPlayer.Builder(requireContext()).build()
+        video_fragment_video_view.player = player
+        player.setMediaItem(MediaItem.fromUri(url))
+        player.playWhenReady = playWhenReady
+        player.seekTo(playBackPosition)
+        player.prepare()
+    }
+
+    private fun releasePlayer() {
+        video_fragment_video_view?.player?.let {
+            playWhenReady = it.playWhenReady
+            playBackPosition = it.currentPosition
+            it.release()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        video_fragment_video_view.player?.let {
+            outState.putLong(
+                PLAYBACK_TIME,
+                it.currentPosition
+            )
+            outState.putBoolean(
+                PLAY_WHEN_READY,
+                it.playWhenReady
+            )
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N)
+            releasePlayer()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+            releasePlayer()
+    }
 }
---
+++ app/src/main/java/com/example/dentalyear/view/adapter/VideoAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.dentalyear.view.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dentalyear.R
+import com.example.dentalyear.data.model.VideoModel
+
+class VideoAdapter(private var data: List<VideoModel>? = null): RecyclerView.Adapter<VideoAdapter.VideoViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideoViewHolder {
+        return VideoViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.recycler_view_video_list_item, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: VideoViewHolder, position: Int) {
+        holder.videoTitleTextView.text = data?.get(position)?.videoTitle
+        holder.durationTextView.text = data?.get(position)?.videoDuration
+    }
+
+    override fun getItemCount() = data?.size ?: 0
+
+    fun setData(newData:List<VideoModel>?){
+        data = newData
+        notifyDataSetChanged()
+    }
+
+    class VideoViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val videoTitleTextView: TextView =
+            itemView.findViewById(R.id.video_recycler_view_title_text_view)
+
+        val durationTextView: TextView =
+            itemView.findViewById(R.id.video_recycler_view_duration_text_view)
+    }
+}
